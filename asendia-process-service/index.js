48caebe893217e80426239f7202f96bf504a66fc
Joe Marcus <josephcmarcus@gmail.com>
2023-12-06 14:13:01 -0500

    Add functionality to pull from database

--- asendia-process-service/index.js
+++ asendia-process-service/index.js
@@ -1,13 +1,19 @@
 ﻿const df = require("durable-functions");
 
 module.exports = df.orchestrator(function* (context) {
+    const activityPayload = {
+        instanceId: context.df.instanceId,
+        data: [],
+        errors: [],
+    }
+
     const outputs = [];
 
-    // Replace "Hello" with the name of your Durable Activity Function.
-    outputs.push(yield context.df.callActivity("Hello", "Tokyo"));
-    outputs.push(yield context.df.callActivity("Hello", "Seattle"));
-    outputs.push(yield context.df.callActivity("Hello", "London"));
+    const data = yield context.df.callActivity('getData', activityPayload);
 
-    // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+    activityPayload.data = data;
+
+    outputs.push(data);
+
     return outputs;
 });
---
+++ database.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+const fs = require('fs');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    ca: fs.readFileSync(__dirname + process.env.CRT),
+  },
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const promisePool = pool.promise();
+
+module.exports.getData = async function (table) {
+  // returns an array of records matching the sql query, or an empty array if no records are found
+  const [rows] = await promisePool.query(
+    `SELECT * FROM ${table}`
+  );
+  return rows;
+};
