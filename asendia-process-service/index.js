505ff9b0cdaf5f74c7275f2dc2b4480d314f1519
Joe Marcus <josephcmarcus@gmail.com>
2023-12-06 13:29:38 -0500

    Install dotenv and mysql2 as dependencies

---
+++ asendia-process-service/index.js
@@ -0,0 +1,13 @@
+﻿const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const outputs = [];
+
+    // Replace "Hello" with the name of your Durable Activity Function.
+    outputs.push(yield context.df.callActivity("Hello", "Tokyo"));
+    outputs.push(yield context.df.callActivity("Hello", "Seattle"));
+    outputs.push(yield context.df.callActivity("Hello", "London"));
+
+    // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+    return outputs;
+});
